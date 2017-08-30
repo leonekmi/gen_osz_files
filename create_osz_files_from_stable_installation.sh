c8c021febca9f6fd39c2da3bf57b6dc40c37c6a2
leonekmi <leonekmi@hotmail.com>
2017-08-30 21:22:39 +0200

    Update create_osz_files_from_stable_installation.sh

--- create_osz_files_from_stable_installation.sh
+++ create_osz_files_from_stable_installation.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            true
+            ;;
+        *)
+            exit
+            ;;
+    esac
+}
+echo "Hi, this script will generate .osz from ALL beatmaps in Songs/ folder (you must cd in the Songs/ folder or it will not work)"
+confirm "Are you into Songs/ folder ? [y/N]"
+FOLDERS="$(ls -l | grep -c ^d)"
+echo "${FOLDERS} beatmaps detected, processing all"
+sleep 1
+mkdir osz
 for d in */
 do
-    tput setaf 2
-    echo "Processing ${d::-1}"
-    tput sgr 0
-    ( cd "$d" && zip -r "${d::-1}.osz" * && mv "${d::-1}.osz" ../ )
+    if [ "$d" != "osz/" ]
+    then
+        tput setaf 2
+        echo "Processing ${d::-1}"
+        tput sgr 0
+        ( cd "$d" && zip -r "${d::-1}.osz" * && mv "${d::-1}.osz" ../osz )
+    fi
 done
+tput setaf 2
+echo "Done ! All beatmaps are processed and are in osz/ folder created just now ! (Note : don't run osu! to avoid re-processing of beatmaps, move the folder elsewhere)"
+tput sgr 0
