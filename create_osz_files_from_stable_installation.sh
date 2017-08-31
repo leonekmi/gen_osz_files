02a372e650ff77603f96e1f0eb54a76ebefb3ffc
leonekmi <leonekmi@hotmail.com>
2017-08-31 12:25:23 +0200

    Begin auto import func

--- create_osz_files_from_stable_installation.sh
+++ create_osz_files_from_stable_installation.sh
@@ -31,3 +31,15 @@ done
 tput setaf 2
 echo "Done ! All beatmaps are processed and are in osz/ folder created just now ! (Note : don't run osu! to avoid re-processing of beatmaps, move the folder elsewhere)"
 tput sgr 0
+confirm "Do you want to try to automatically import the beatmaps to osu!lazer ? (Arch Linux with osu-lazer-git from aur only) [y/N]"
+ISARCHINSTALLED="$(uname -r|grep ARCH)"
+REALPATH="$(realpath osz)/*"
+echo "$REALPATH"
+if [[ "$ISARCHINSTALLED" = *"ARCH"* ]]
+then
+    ( pacman -Qi osu-lazer-git > /dev/null && tput setaf 2 && echo "osu-lazer-git package detected, importing beatmaps !" && echo "osu-lazer will be launched in 3 seconds and beatmaps will be imported (it may takes a while)" && tput sgr 0 && sleep 3 && mono /usr/lib/osu-lazer/osu\!.exe $REALPATH  || tput setaf 4 && echo "osu-lazer-git not detected from pacman, can't import" && tput sgr 0 )
+else
+    tput setaf 3
+    echo "It seems that you don't have ArchLinux installed (detected by uname -r)"
+    tput sgr 0
+fi
